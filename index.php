0a6cac4d87a7607d3fcfb8185b1445d237efa714
Jaroslav Týc <mail@jaroslavtyc.com>
2019-05-06 09:05:04 +0200

    Update script reports more details and in JSON format

--- index.php
+++ index.php
@@ -1,19 +1,67 @@
 <?php
 error_reporting(-1);
-ini_set('display_errors', '1');
+ini_set('display_errors', '0');
 require_once __DIR__ . '/vendor/autoload.php';
 
-echo implode(
-    "\n",
-    (new Granam\Git\Git())->update(__DIR__)
-);
+header('Content-Type: application/json');
+header('Cache-Control: no-cache');
 
-passthru('composer install --optimize-autoloader --no-interaction 2>&1', $return);
-if ($return !== 0) {
-    throw new RuntimeException('Can not install libraries via Composer');
+$output = [];
+$exitCode = 0;
+
+$start = new DateTimeImmutable();
+$codeUpdate = ['start' => new DateTimeImmutable()];
+try {
+    $codeUpdate['result'] = (new Granam\Git\Git())->update(__DIR__);
+} catch (Throwable $throwable) {
+    $codeUpdate['error'] = $throwable->getTraceAsString();
+    $exitCode = 1;
+}
+$codeUpdate['end'] = new DateTimeImmutable();
+$codeUpdate['duration'] = $codeUpdate['end']->diff($codeUpdate['start']);
+$codeUpdate['exit_code'] = $exitCode;
+$output['code_update'] = $codeUpdate;
+if ($exitCode > 0) {
+    echo json_encode($output, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    exit($exitCode);
 }
 
-passthru(__DIR__ . '/vendor/bin/statie generate source 2>&1', $return);
-if ($return !== 0) {
-    throw new RuntimeException('Can not generate output by Statie');
+$composerInstall = ['start' => new DateTimeImmutable()];
+exec('composer install --optimize-autoloader --no-interaction 2>&1', $composerInstallOutput, $exitCode);
+if ($exitCode === 0) {
+    $composerInstall['result'] = $composerInstallOutput;
+} else {
+    $composerInstall['error'] = $composerInstallOutput;
 }
+$composerInstall['end'] = new DateTimeImmutable();
+$composerInstall['duration'] = $composerInstall['end']->diff($composerInstall['start']);
+$composerInstall['exit_code'] = $exitCode;
+$output['composer_install'] = $composerInstall;
+if ($exitCode > 0) {
+    echo json_encode($output, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    exit($exitCode);
+}
+
+$outputGenerating = ['start' => new DateTimeImmutable()];
+exec(__DIR__ . '/vendor/bin/statie generate source 2>&1', $outputGeneratingOutput, $exitCode);
+$outputGeneratingOutput = array_filter($outputGeneratingOutput, function (string $line) {
+    return trim($line) !== '';
+});
+if ($exitCode === 0) {
+    $outputGenerating['result'] = $outputGeneratingOutput;
+} else {
+    $outputGenerating['error'] = $outputGeneratingOutput;
+}
+$outputGenerating['end'] = new DateTimeImmutable();
+$outputGenerating['duration'] = $outputGenerating['end']->diff($outputGenerating['start']);
+$outputGenerating['exit_code'] = $exitCode;
+$output['output_generating'] = $outputGenerating;
+
+$outputGenerating['total_duration'] = (new DateTimeImmutable())->diff($start);
+$error = error_get_last();
+if ($error) {
+    $outputGenerating['error'] = $error;
+}
+
+echo json_encode($output, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+exit($exitCode);
