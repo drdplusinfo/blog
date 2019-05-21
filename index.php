087a07922b744a78bea67edc6e1ec58401078cab
Jaroslav Týc <mail@jaroslavtyc.com>
2019-05-21 12:43:57 +0200

    Output is re-generated on update

--- index.php
+++ index.php
@@ -13,13 +13,35 @@ $codeUpdate = ['start' => new DateTimeImmutable()];
 try {
     $codeUpdate['result'] = (new Granam\Git\Git())->update(__DIR__);
 } catch (Throwable $throwable) {
-    $codeUpdate['error'] = $throwable->getTraceAsString();
+    $codeUpdate['error'] = $throwable->getMessage() . '; ' . var_export($throwable->getTrace(), true);
     $exitCode = 1;
 }
 $codeUpdate['end'] = new DateTimeImmutable();
 $codeUpdate['duration'] = $codeUpdate['end']->diff($codeUpdate['start']);
 $codeUpdate['exit_code'] = $exitCode;
 $output['code_update'] = $codeUpdate;
+
+if ($exitCode !== 0) {
+    echo json_encode($output, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    exit($exitCode);
+}
+
+$outputGeneration = ['start' => new DateTimeImmutable()];
+exec('vendor/bin/statie generate source', $generationOutput, $generationCode);
+$generationOutput = array_filter($generationOutput, function (string $row) {
+    return trim($row) !== '';
+});
+$generationOutput = implode("\n", $generationOutput);
+if ($generationCode !== 0) {
+    $outputGeneration['error'] = $generationOutput;
+    $exitCode = $generationCode;
+} else {
+    $outputGeneration['result'] = $generationOutput;
+}
+$outputGeneration['end'] = new DateTimeImmutable();
+$outputGeneration['duration'] = $outputGeneration['end']->diff($outputGeneration['start']);
+$outputGeneration['exit_code'] = $exitCode;
+$output['output_generation'] = $outputGeneration;
 
 echo json_encode($output, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 exit($exitCode);
